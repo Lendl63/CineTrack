a5ee5ed8d5a256bc41062ed2c5bcd3dd1b2415b3
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-05-21 22:08:46 +0100

    feat: ajout du code java de la vue

--- app/src/main/java/com/tp/cinetrack/SplashActivity.java
+++ app/src/main/java/com/tp/cinetrack/SplashActivity.java
@@ -1,4 +1,25 @@
 package com.tp.cinetrack;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+
+    // Affichage pendant 2.5 secondes
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Ferme le splash
+      }
+    }, 3500);
+  }
 }
