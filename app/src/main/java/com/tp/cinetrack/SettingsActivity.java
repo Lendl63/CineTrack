ba2d80760df9533c9cce1cba96c1027bf56bc3e1
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-05-22 10:35:28 +0100

    feat: ajout de la fonctionalite de base de l'interface

--- app/src/main/java/com/tp/cinetrack/SettingsActivity.java
+++ app/src/main/java/com/tp/cinetrack/SettingsActivity.java
@@ -1,4 +1,29 @@
 package com.tp.cinetrack;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SettingsActivity extends AppCompatActivity {
+
+
+  private ImageView btnBack;
+  private RecyclerView rvSearchResults;
+  private TextView tvEmptySearch;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+
+    btnBack = findViewById(R.id.btn_back);
+    // Bouton retour
+    btnBack.setOnClickListener(v -> finish());
+  }
 }
+
