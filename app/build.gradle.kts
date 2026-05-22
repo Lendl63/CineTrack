149796207f6a69035ef5377cc68d8d33aa54fa19
Lendl63 <212922348+Lendl63@users.noreply.github.com>
2026-05-22 10:25:51 +0100

    feat: ajout de dependances

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.android.material:material:1.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
---
+++ app/src/main/java/com/tp/cinetrack/SettingsActivity.java
@@ -0,0 +1,4 @@
+package com.tp.cinetrack;
+
+public class SettingsActivity {
+}
